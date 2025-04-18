a0ed0f198553496b0935b5715ed0c12aa7a7d516
Ali Mousavi <amousavig@icloud.com>
2025-04-18 13:23:58 +0330

    Added functions for id encryption and decryption.

---
+++ lib/server-utils.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import crypto from "crypto";
+
+const algorithm = "aes-256-cbc";
+const secretKey = crypto
+  .createHash("sha256")
+  .update(process.env.NEXTAUTH_SECRET!)
+  .digest();
+const ivLength = 16;
+
+export async function encryptId(id: string) {
+  const iv = crypto.randomBytes(ivLength);
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+  let encrypted = cipher.update(id, "utf-8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted;
+}
+
+export async function decryptId(encryptedId: string) {
+  const [ivHex, encrypted] = encryptedId.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+  let decrypted = decipher.update(encrypted, "hex", "utf-8");
+  decrypted += decipher.final("utf-8");
+  return decrypted;
+}
