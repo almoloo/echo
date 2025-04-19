b364cb2182c9fb9e2be9fcee94605f0fff149311
Ali Mousavi <amousavig@icloud.com>
2025-04-20 01:18:37 +0330

    Added up provider.

--- app/widget/up/page.tsx
+++ app/widget/up/page.tsx
@@ -1,3 +1,70 @@
+"use client";
+
+import {
+  createClientUPProvider,
+  type UPClientProvider,
+} from "@lukso/up-provider";
+import { useCallback, useEffect, useState } from "react";
+
+let provider: UPClientProvider;
+
 export default function UpWidgetPage() {
-  return <div>Hello UP</div>;
+  const [accounts, setAccounts] = useState<Array<`0x${string}`>>([]);
+  const [contextAccounts, setContextAccounts] = useState<Array<`0x${string}`>>(
+    []
+  );
+  const [profileConnected, setProfileConnected] = useState(false);
+
+  const updateConnected = useCallback(
+    (
+      _accounts: Array<`0x${string}`>,
+      _contextAccounts: Array<`0x${string}`>
+    ) => {
+      setProfileConnected(_accounts.length > 0 && _contextAccounts.length > 0);
+    },
+    []
+  );
+
+  useEffect(() => {
+    provider = createClientUPProvider();
+
+    async function init() {
+      try {
+        const _accounts = provider.accounts as Array<`0x${string}`>;
+        setAccounts(_accounts);
+
+        const _contextAccounts = provider.contextAccounts;
+        updateConnected(_accounts, _contextAccounts);
+      } catch (error) {
+        console.error("Failed to initialize provider: ", error);
+      }
+    }
+
+    const accountsChanged = (_accounts: Array<`0x${string}`>) => {
+      setAccounts(_accounts);
+      updateConnected(_accounts, contextAccounts);
+    };
+
+    const contextAccountsChanged = (_accounts: Array<`0x${string}`>) => {
+      setContextAccounts(_accounts);
+      updateConnected(accounts, _accounts);
+    };
+
+    init();
+
+    provider.on("accountsChanged", accountsChanged);
+    provider.on("contextAccountsChanged", contextAccountsChanged);
+
+    return () => {
+      provider.removeListener("accountsChanged", accountsChanged);
+      provider.removeListener("contextAccountsChanged", contextAccountsChanged);
+    };
+  }, [accounts[0], contextAccounts[0], updateConnected]);
+  return (
+    <div>
+      Hello UP
+      <div>account: {accounts[0]}</div>
+      <div>context: {contextAccounts[0]}</div>
+    </div>
+  );
 }
