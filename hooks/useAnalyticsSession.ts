bae0e22e07d31b992c14555eb14548fb3dcedcc2
Ali Mousavi <amousavig@icloud.com>
2025-05-01 00:39:47 +0330

    Added date to Visitor data.

--- hooks/useAnalyticsSession.ts
+++ hooks/useAnalyticsSession.ts
@@ -56,6 +56,7 @@ export function useAnalyticsSession(
         sessionId: sessionId!,
         location: ipInfo,
         contextWallet: contextWalletAddress,
+        date: Date.now(),
       };
 
       setHasSessionInit(true);
