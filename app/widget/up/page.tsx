206df87703eb085250f3adf39449106555f86f6c
Ali Mousavi <amousavig@icloud.com>
2025-05-20 14:37:46 +0330

    test.

--- app/widget/up/page.tsx
+++ app/widget/up/page.tsx
@@ -47,8 +47,6 @@ export default function UpWidgetPage() {
         //   init();
         // }, 3000);
         console.error("init loading error: ", error);
-      } finally {
-        setInitLoading(false);
       }
     }
 
